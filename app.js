44da4bcfe28ac5ba9e8042da94ddd2c339a56f9d
Tijan <tijanmdr@gmail.com>
2016-10-26 13:00:23 +0545

    First Message

---
+++ app.js
@@ -0,0 +1,49 @@
+var app = new Vue({
+	el: '#events', 
+	data: {
+		event: {name: '', description: '', date: ''},
+		events: []
+	}, 
+	mounted: function () {
+		this.fetchEvents();
+	}, 
+	methods: {
+		fetchEvents: function () {
+			var events = [
+				{
+					id: 1,
+					name: 'TIFF',
+					description: 'Lorem Ipsum Dolores',
+					date: '2015-09-02'
+				},
+				{
+					id: 2,
+					name: 'React.js',
+					description: 'Tutorials',
+					date: '2016-03-26'
+				}, 
+				{
+					id: 3,
+					name: 'Node.js',
+					description: 'Backend Tutorials',
+					date: '2015-12-02'
+				}
+			];
+
+			this.events = events;
+		}, alertNow: function () {
+			alert("");
+		}, 
+		addEvent: function () {
+			if (this.event.name) {
+				this.events.push(this.event);
+				this.event = {name: '', description: '', date: ''};
+			}
+		}, 
+		deleteEvent: function (key) {
+			if (confirm("Are you sure you want to delete")) {
+				this.events.splice(key,1);
+			}
+		}
+	}
+});
